3af96ea7b89fbcb6ddcca381f26d3ed516b106de
legik777 <75944488+legik777@users.noreply.github.com>
2020-12-21 21:41:14 +0300

    Update header.hpp

--- include/header.hpp
+++ include/header.hpp
@@ -1,6 +1,59 @@
-// Copyright 2020 Your Name <your_email>
+// Copyright 2020 Andreytorix
+#pragma once
+#include <limits.h>
+#include <boost/filesystem/operations.hpp>
+#include <boost/algorithm/string/predicate.hpp>
+#include <boost/algorithm/string/split.hpp>
+#include "boost/algorithm/string.hpp"
+#include <vector>
+#include <iostream>
+#include <string>
+#include <any>
 
-#ifndef INCLUDE_HEADER_HPP_
-#define INCLUDE_HEADER_HPP_
-
-#endif // INCLUDE_HEADER_HPP_
+class Broker {
+public:
+    static std::vector<Broker> brokerVector;
+    std::string catalog;
+    std::string id;
+    int date;
+    int counter;
+    static void AddBroker(std::string inputStringBroker) {
+        std::vector<std::string> v;
+        boost::split(v, inputStringBroker, boost::is_any_of(" ,._"));
+        checkBroker(v);
+    }
+    static std::vector<Broker> ListBrokersv() {
+        return (brokerVector);
+    }
+    std::string toString() {
+        return("broker:"+ this -> catalog + " account:" + this -> id + " files:" + std::to_string (this ->counter) + " lastdate:" + std::to_string (this -> date));
+    }
+   static void clearVector() {
+        brokerVector.clear();
+    }
+private:
+    static void checkBroker(std::vector<std::string> inputBroker) {
+        bool exists = false;
+        for (int i = 0; i < brokerVector.size(); i++) {
+            Broker* br = &brokerVector[i];
+            if (br->id == inputBroker[2]) {
+                exists = true;
+                if (std::stoi(inputBroker[3]) > br->date) {
+                    br->date = std::stoi(inputBroker[3]);
+                }
+                br->counter++;
+            }
+        }
+        if (!exists) {
+            Broker broker;
+            broker.counter = 1;
+            broker.catalog = inputBroker[0];
+            broker.id = inputBroker[2];
+            broker.date = std::stoi(inputBroker[3]);
+            brokerVector.push_back(broker);
+        }
+    }
+};
+std::vector<std::string> recdir(std::string);
+void show_vector(std::vector<std::string> vec);
+void show_broker(std::vector<Broker> vec);
